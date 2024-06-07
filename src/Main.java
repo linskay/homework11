17faa6ca6767fd32fa38c12ee44344af66db6ec3
linskay <alina.cheremiskina@gmail.com>
2024-06-07 21:45:07 +0300

    complete v.2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@ import java.time.LocalDate;
 
 public class Main {
 
-    public static String enterYear(int year) {
-        if ((year > 1584 && year % 4 == 0 && year % 100 != 0) || (year > 1584 && year % 400 == 0))
-            return "Этот год високосный";
-        else
-            return "Год не високосный";
+    public static void checkLeapYear(int year) {
+        if ((year > 1584 && year % 4 == 0 && year % 100 != 0) || (year > 1584 && year % 400 == 0)) {
+            System.out.println("Этот год високосный");
+        } else {
+            System.out.println("Год не високосный");
+        }
     }
 
-    public static void selectOS(int clientOS, int clientDeviceYear) {
+    public static int clientDeviceYear = LocalDate.now().getYear();
+
+    public static void selectOS(int clientOS) {
         if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
@@ -23,17 +26,18 @@ public class Main {
     }
 
     public static String returnNumberDaysDelivery(int deliveryDistance) {
-        boolean distance1 = (deliveryDistance < 20);
-        if (distance1)
+        if (deliveryDistance < 20) {
             return "Потребуется один день";
-        boolean distance2 = (deliveryDistance < 60 && deliveryDistance >= 20);
-        if (distance2)
+        }
+        if (deliveryDistance < 60 && deliveryDistance >= 20) {
             return "Потребуется два дня";
-        boolean distance3 = (deliveryDistance <= 100 && deliveryDistance >= 60);
-        if (distance3)
+        }
+        if (deliveryDistance <= 100 && deliveryDistance >= 60) {
             return "Потребуется три дня";
-        if (deliveryDistance > 100)
+        }
+        if (deliveryDistance > 100) {
             return "Доставки нет";
+        }
         return "";
     }
 
@@ -41,11 +45,11 @@ public class Main {
         System.out.println("Привет, наставник!:)");
         System.out.println("Task 1");
         int year = 2024;
-        System.out.println(year + " - " + enterYear(year));
+        System.out.printf(year + " - ");
+        checkLeapYear(year);
         System.out.println("Task 2");
         int clientOS = 1;
-        int clientDeviseYear = LocalDate.now().getYear();
-        selectOS(clientOS, clientDeviseYear);
+        selectOS(clientOS);
         System.out.println("Task 3");
         int deliveryDistance = 100;
         System.out.println(returnNumberDaysDelivery(deliveryDistance));
